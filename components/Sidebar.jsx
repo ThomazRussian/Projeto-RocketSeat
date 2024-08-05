d5b1963c8b4394832a1f69603fbd818fc713294d
Thomaz Russian <Thomazrussian@gmail.com>
2024-08-04 21:19:15 -0300

    Feat: Sidebar/Profile.

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from '@phosphor-icons/react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,9 +7,8 @@ export function Sidebar() {
             <img className={styles.cover}
                 src=" https://images.unsplash.com/photo-1511376777868-611b54f68947?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                 />
-
-            
             <div className={styles.profile}>
+                <img className={styles.avatar} src="http://github.com/ThomazRussian.png"/>
                 <strong>Thomaz Russian</strong>
                 <span> Web Developer</span>
 
@@ -16,10 +16,11 @@ export function Sidebar() {
 
             <footer>
                 <a href="#">
+                    <PencilLine size= {32} 
+                    />
                 Editar seu Perfil
                 </a>
             </footer>
-
         </aside>
     );
 }

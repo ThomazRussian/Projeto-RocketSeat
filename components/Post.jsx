0f2d4451d9904fce5d06af57fbc2ad7c96773319
Thomaz Russian <Thomazrussian@gmail.com>
2024-08-15 17:00:25 -0300

    Fix: Post.jsx

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,32 +1,26 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post () {
+export function Post ({ author, publishedAt }) {
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/diego3g.png" 
-                    />
-                <div className={styles.authorInfo}>
-                    <strong>Thomaz Russian </strong>
-                    <span>Web Developer</span>
+                    <Avatar src={author.avatarUrl} />
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
                 </div>
-                </div>
-                <time title ="11 de Maio ás 08:13h" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+                <time title ="11 de Maio ás 08:13h" dateTime='2022-05-11 08:13:30'>
+                    {publishedAt}
+                </time>
             </header>
 
-        <div className={styles.content}>  
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p><a href="">jane.design/doctorcare</a></p>
-            <p> 
-                <a href="">#novoprojeto</a>{' '}
-                <a href="">#nlw</a>{' '}
-                <a href="">#rocketseat</a>
-            </p>
-        </div>
+        <div className={styles.content}>
+            </div>
 
         <form className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>

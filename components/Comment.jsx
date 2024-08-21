35dc25d672265f8788c274142fc2b0dcc7b0dfae
Thomaz Russian <Thomazrussian@gmail.com>
2024-08-21 17:04:01 -0300

    Feat: Delete Comments

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment}) {
+
+    function handleDeleteComment() {
+
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}> 
             <Avatar hasBorder={false} src="https://github.com/ThomazRussian.png" />
@@ -15,7 +21,7 @@ export function Comment({ content }) {
                         <time title ="11 de Maio ás 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                     </div>
 
-                    <button title="Deletar Comentário">
+                    <button onClick={handleDeleteComment} title="Deletar Comentário">
                         <Trash size={24} />
                     </button>
                 </header>

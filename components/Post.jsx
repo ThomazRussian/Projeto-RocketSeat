39a774b284f25a7a999d3e4da647544bd9002c34
Thomaz Russian <Thomazrussian@gmail.com>
2024-08-23 17:37:26 -0300

    Feat: delete comments

--- components/Post.jsx
+++ components/Post.jsx
@@ -25,24 +25,33 @@ export function Post ({ author, publishedAt, content }) {
         addSuffix: true,
     })
 
-    function handleCreateNewComment(){
+    function handleCreateNewComment() {
         event.preventDefault()
 
-        //Imutabilidade.
         setComments([...comments, newCommentText]);
         setNewCommentText('');
     }
 
-    function handleNewCommentChange(){
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
 
-    function deleteComment(comment){
-
-
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
 
     }
 
+    function deleteComment(commentToDelete){
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
+    
     return(
         <article className={styles.post}>
             <header>
@@ -76,10 +85,14 @@ export function Post ({ author, publishedAt, content }) {
                 placeholder ="Deixe um comentario"
                 value={newCommentText}
                 onChange={handleNewCommentChange}
+                onInvalid={handleNewCommentInvalid}
+                required
             />
 
             <footer>
-                <button type="submit">Publicar</button>
+                <button type="submit" disabled={isNewCommentEmpty}>
+                    Publicar
+                </button>
             </footer>
         </form>
 
